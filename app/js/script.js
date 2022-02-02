75c26183e4d8ffcb8a849eb514ef4296fa660c18
MoJo <Jvarughese1994@gmail.com>
2022-02-01 20:36:08 -0600

    button functionality added, minor improvemnts in formatting

--- app/js/script.js
+++ app/js/script.js
@@ -1,22 +1,87 @@
 const main = document.querySelector('#main');
 const header = document.querySelector('header');
-const links = document.querySelectorAll('.link');
+const contentTitle = document.querySelector('.content-title');
 
-links.forEach(function(link) {
-  link.addEventListener('click', function() {
-    if (main.classList.contains('open')) {
-      main.classList.remove('open');
-      //header.classList.remove('fade-out');
-      //header.classList.add('fade-in');
-      main.classList.remove('fade-in');
-      main.classList.add('fade-out');
-    } else {
+const aboutMeBtn = document.querySelector('.about-me-btn');
+const programingBtn = document.querySelector('.programing-btn');
+const artBtn = document.querySelector('.art-btn');
+const blogBtn = document.querySelector('.blog-btn');
+
+const mobileBackBtn = document.querySelector('.back');
+
+const aboutMeContent = document.querySelector('.about-me-content');
+const programingContent = document.querySelector('.programing-content');
+const artContent = document.querySelector('.art-content');
+const blogContent = document.querySelector('.blog-content');
+
+  mobileBackBtn.addEventListener('click', function() {
+    CloseAllContent(null);
+    ToggleMain(false);
+  });
+
+  aboutMeBtn.addEventListener('click', function() {
+    contentTitle.innerHTML = 'About Me';
+    CheckOpenContent(aboutMeContent);
+  });
+  programingBtn.addEventListener('click', function() {
+    contentTitle.innerHTML = 'Programing';
+    CheckOpenContent(programingContent);
+  });
+  artBtn.addEventListener('click', function() {
+    contentTitle.innerHTML = 'Art';
+    CheckOpenContent(artContent);
+  });
+  blogBtn.addEventListener('click', function() {
+    contentTitle.innerHTML = 'Blog';
+    CheckOpenContent(blogContent);
+  });
+
+  function CheckOpenContent(content) {
+    if (!main.classList.contains('open')) ToggleMain(true);
+    CloseAllContent(content);
+    if (!content.classList.contains('fade-in')) {
+      content.classList.remove('fade-out');
+      content.classList.add('fade-in');
+    }
+  }
+
+  function CloseAllContent(content) {
+    CheckCloseContent(aboutMeContent);
+    CheckCloseContent(programingContent);
+    CheckCloseContent(artContent);
+    CheckCloseContent(blogContent);
+    setTimeout(() => {DisableAllContent(content)}, 230);
+  }
+
+  function DisableAllContent(content) {
+    DisableContent(aboutMeContent);
+    DisableContent(programingContent);
+    DisableContent(artContent);
+    DisableContent(blogContent);
+    if(content !== null) content.style.setProperty('display', 'block');
+  }
+
+  function DisableContent(content) {
+    content.style.setProperty('display', 'none');
+  }
+
+  function CheckCloseContent(content) {
+    if (content.classList.contains('fade-in')) {
+      content.classList.remove('fade-in');
+      content.classList.add('fade-out');
+    }
+  }
+
+  function ToggleMain(tf) {
+    if (tf) {
       main.classList.add('open');
-      //header.classList.remove('fade-in');
-      //header.classList.add('fade-out');
       main.classList.remove('fade-out');
       main.classList.add('fade-in');
     }
-  });
-});
+    else {
+      main.classList.remove('open');
+      main.classList.remove('fade-in');
+      main.classList.add('fade-out');
+    }
+  }
 
